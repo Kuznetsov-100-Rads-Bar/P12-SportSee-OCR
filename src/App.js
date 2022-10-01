181b0ec4b79253321c7616cf62b92f9828bec355
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-10-01 23:35:04 +0200

    example axios request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Dashboard from './pages/Dashboard';
+import React, { useEffect } from "react";
+import Header from "./components/Header/Header";
+import Dashboard from "./pages/Dashboard";
+
+import axios from "axios";
+
 export default function App() {
+  // exemple d'une requete qui récupere les performances de l'utilisateur
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/user/12/performance")
+      .then((res) => console.log(res.data.data));
+  }, []);
+
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
       <Dashboard />
     </div>
-  )
+  );
 }
