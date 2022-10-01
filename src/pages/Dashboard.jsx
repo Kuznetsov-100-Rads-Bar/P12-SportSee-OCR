1bc7b5a44d4766a378177f01ab93235ca60b6c56
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-10-01 18:31:49 +0200

    mise en place du static

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import Sidebar from '../components/Sidebar/Sidebar';
 export default function Dashboard() {
   return (
     <StyledDashboard>
+      <DashboardContainer> 
       <DashboardLeft>
         <Sidebar />
       </DashboardLeft>
@@ -12,18 +13,28 @@ export default function Dashboard() {
         <DashboardProfile>Bonjour </DashboardProfile><DashboardProfilUserName>Thomas</DashboardProfilUserName>
         <DashboardSlogan>Félicitation ! Vous avez explosé vos objectifs hier 👏</DashboardSlogan>
       </DashboardRight>
+      </DashboardContainer>
     </StyledDashboard>
   )
 }
 
 const StyledDashboard = styled.section`display: flex`;
 
+const DashboardContainer = styled.div`display: flex;
+  gap: 30px`;
+
 const DashboardLeft = styled.aside``;
 
 const DashboardRight = styled.div``;
 
-const DashboardProfile = styled.h1``;
+const DashboardProfile = styled.h1`
+  margin: 0;
+  font-size: 3rem;
+  font-family: "Roboto", sans-serif`;
 
-const DashboardProfilUserName = styled.span`color: #ff0101`;
+const DashboardProfilUserName = styled.span`
+color: #ff0101`;
 
-const DashboardSlogan = styled.p``;
+const DashboardSlogan = styled.p`
+  margin: 20px 0 0 0;
+  font-family: "Roboto", sans-serif`;
